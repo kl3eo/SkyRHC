f0ecaef23603214546063ee7a8e174362a05a4be
kl3eo <alex@motivation.ru>
2022-12-31 21:18:28 +0300

    fix browser links

--- utils/dashboard/src/utils/explorerGuide.ts
+++ utils/dashboard/src/utils/explorerGuide.ts
@@ -1,29 +1,44 @@
 
 const urlBuilderBlockNumber = (value: string, chain: string, provider: string): any => {
-  if (provider === 'subscan') {
+  if (provider === 'subscan' && chain !== 'skypirl') {
     return `https://${chain}.${provider}.io/block/${value}`
   }
 
+  if (provider === 'subscan' && chain === 'skypirl') {
+    value = value.split(',').join('')
+    return `https://${provider}.${chain}.org/skypirl4/block/${value}`
+  }
+  
   if (provider === 'polkascan') {
     return `https://${provider}.io/pre/${chain}/block/${value}`
   }  
 }
 
 const urlBuilderAccount = (value: string, chain: string, provider: string): any => {
-  if (provider === 'subscan') {
+  if (provider === 'subscan' && chain !== 'skypirl') {
     return `https://${chain}.${provider}.io/account/${value}`
   }
 
+  if (provider === 'subscan' && chain === 'skypirl') {
+    value = value.split(',').join('')
+    return `https://${provider}.${chain}.org/skypirl4/account/${value}`
+  }
+  
   if (provider === 'polkascan') {
     return `https://${provider}.io/pre/${chain}/account/${value}`
   }  
 }
 
 const urlBuilderTransaction = (value: string, chain: string, provider: string): any => {
-  if (provider === 'subscan') {
+  if (provider === 'subscan' && chain !== 'skypirl') {
     return `https://${chain}.${provider}.io/extrinsic/${value}`
   }
 
+  if (provider === 'subscan' && chain === 'skypirl') {
+    value = value.split(',').join('')
+    return `https://${provider}.${chain}.org/skypirl4/extrinsic/${value}`
+  }
+  
   if (provider === 'polkascan') {
     return `https://${provider}.io/pre/${chain}/transaction/${value}`
   }
