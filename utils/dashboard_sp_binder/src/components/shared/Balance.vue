b019fafb3487c6e721e4c39cfc5a8fb28fd8c813
kl3eo <alex@motivation.ru>
2023-05-20 22:09:57 +0300

    fix free balance calc

--- utils/dashboard_sp_binder/src/components/shared/Balance.vue
+++ utils/dashboard_sp_binder/src/components/shared/Balance.vue
@@ -2,6 +2,7 @@
   <!-- // TODO denomination and set asset by network -->
 	<b-tag class="balance-tag" 
 		type="is-dark" size="is-medium">
+
     <Money :value="currentBalance" inline />
 	</b-tag>
 </template>
@@ -21,12 +22,15 @@ export default class Balance extends Vue {
   @Prop() public account!: string;
   private currentBalance: string = '0'; 
 
-
   @Watch('account')
   public async onAccountChange(value: string) {
     const { api } = Connector.getInstance();
     const { nonce, data: balance } = await api.query.system.account(value);
-    this.currentBalance = balance.free?.toString();
+
+    let a = parseFloat(balance.free?.toString());
+    let b = parseFloat(balance.miscFrozen?.toString());
+    let c = a - b;
+    this.currentBalance = c?.toString();
   }
 }
 </script>
